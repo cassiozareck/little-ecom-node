6bf14dd6a951f5ba67d9dc3dd1fae8d838a23cf1
Cassio Zareck <cassiomilczareck@gmail.com>
2024-01-27 18:21:48 -0300

    buyproduct and updateproduct endpoints

--- backend.js
+++ backend.js
@@ -25,6 +25,65 @@ module.exports = function(app, connection, checkLogin) {
         });
     })
 
+    // GetProducts will return all products
+    app.get('/products', (req, res) => {
+        const query = 'SELECT * FROM products';
+        connection.query(query, (err, results) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).send('Internal Server Error');
+            }
+
+            console.log('Found', results.length, 'items');
+            res.json(results);
+        });
+    });
+
+    // UpdateProduct will update a product by its ID
+    app.put('/product/:id', checkLogin, (req, res) => {
+        const id = req.params.id;
+        const { price, name } = req.body;
+
+        // Validate the product data
+        if (price < 0 || price > 99999 || name === '') {
+            return res.status(400).send('Invalid product data');
+        }
+
+        const query = 'UPDATE product SET price = ?, name = ? WHERE id = ?';
+        connection.query(query, [price, name, id], (err, results) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).send('Internal Server Error');
+            }
+
+            if (results.affectedRows === 0) {
+                return res.status(404).send('Item not found');
+            }
+
+            console.log('Item updated');
+            res.status(204).send();
+        });
+    });
+
+    // BuyProduct will buy a product by its ID
+    app.post('/product/:id/buy', checkLogin, (req, res) => {
+        const id = req.params.id;
+        const query = 'DELETE FROM product WHERE id = ?';
+        connection.query(query, [id], (err, results) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).send('Internal Server Error');
+            }
+
+            if (results.affectedRows === 0) {
+                return res.status(404).send('Item not found');
+            }
+
+            console.log('Item deleted');
+            res.status(204).send();
+        });
+    });
+
     // AddProduct endpoint will be protected by checkLogin middleware
     app.post('/product', checkLogin, (req, res) => {
         const owner = req.username.split('@')[0];
@@ -63,4 +122,5 @@ module.exports = function(app, connection, checkLogin) {
             res.status(204).send();
         });
     });
+
 }
